1e4b9d7302861312fd2ffd05a02dd1157eefcd77
leeway64 <68257712+leeway64@users.noreply.github.com>
2020-08-30 10:43:36 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# By Lee-Way Wang
+# 30 July 2020
+
+# This program implements the real time listening capability of the virtual assistant. It also implements text
+# input capability to the virtual assistant
+# This program also allows the user to chat with a simple chat bot
+import virtual_assistant_functions
+
+enable_text_input = False
+virtual_assistant_functions.activate_virtual_assistant(enable_text_input)
